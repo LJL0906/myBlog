dded4a4f58aec1aafc1b02370a67db50e1dd91a8
e_liujianlong <e_liujianlong@enn.cn>
2024-08-29 13:51:23 +0800

    我的博客

--- .vitepress/theme/components/DataPanel.vue
+++ .vitepress/theme/components/DataPanel.vue
@@ -9,7 +9,7 @@
           本站总访问量
           <span id="busuanzi_value_site_pv" class="font-bold">--</span> 次
         </span>
-        <img src="../../../public/myPass.svg" alt="heart" width="100" height="100" />
+        <img src="/myPass.svg" alt="heart" width="100" height="100" />
         <span class="text">
           本站访客数
           <span id="busuanzi_value_site_uv" class="font-bold">--</span> 人次
